7811f09abb9a2fa1e3fe2d68cdd7d7aa29c45f2d
admiralbolt <aviknecht@gmail.com>
2023-11-14 23:15:32 -0800

    My cup overflows with rage and sadness. Though I may be broken and overwhelmed, it's a small price, for the circles change size when you zoom in now. Fuck you react leaflet.

--- sms_frontend/src/components/Map/Map.js
+++ sms_frontend/src/components/Map/Map.js
@@ -1,112 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Map.css';
 
-import axios from "axios";
+import MapData from './MapData.js';
 
-import { MapContainer, TileLayer, Marker, Popup, Tooltip, Circle } from 'react-leaflet'
+import axios from 'axios';
+
+import { useMap, useMapEvents, MapContainer, TileLayer, Marker, Popup, Tooltip, Circle } from 'react-leaflet'
 
 const SHOW_COLOR = '#0070ff';
 const OPEN_MIC_COLOR = '#ee6600';
 const NO_EVENT_COLOR = '#989898';
 
-const position = [47.65113, -122.3400]
+const position = [47.65113, -122.3400];
+const zoom = 13;
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      venues: [],
-      events: {}
-    }
-  }
-
-  componentDidMount() {
-    this.loadShows();
-    this.loadVenues();
-  }
-
-  loadVenues() {
-    axios.get("/api/venues").then((res) => {
-      this.setState({ venues: res.data });
-    });
-  }
-
-  loadShows() {
-    // Shows should be indexed by venue. We do a two layer mapping of
-    // venu -> date -> show object.
-    axios.get("/api/events").then((res) => {
-      const events = {};
-      res.data.forEach(event => {
-        events[event.venue] = events[event.venue] || {};
-        events[event.venue][event.event_day] = event;
-      });
-      this.setState({ events: events });
-    });
-  }
-
-  hasShow(venue, date) {
-    if (this.state == undefined) return false;
-    if (this.state.events == undefined) return false;
-    if (!(venue.id in this.state.events)) return false;
-    if (!(date in this.state.events[venue.id])) return false;
-
-    return true;
-  }
-
-  formatTime(t) {
-    return new Date('1970-01-01T' + t + 'Z').toLocaleTimeString('en-US',
-      {timeZone:'UTC', hour12:true, hour:'numeric', minute:'numeric'}
-    );
-  }
-
-  getTicketPrice(event) {
-    if (event.ticket_price_min == event.ticket_price_max)
-      return `$${event.ticket_price_min}`
-
-    return `$${event.ticket_price_min} - $${event.ticket_price_max}`
-  }
-
-  render() {
-    return (
-      <MapContainer center={position} zoom={14} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {this.state.venues.map((venue) => (
-          <Circle
-            key={venue.id}
-            center={[venue.latitude, venue.longitude]}
-            pathOptions={{
-              color: (this.hasShow(venue, this.props.date) ? (this.state.events[venue.id][this.props.date].event_type == 'Open Mic' ? OPEN_MIC_COLOR : SHOW_COLOR) : NO_EVENT_COLOR),
-              fillColor: (this.hasShow(venue, this.props.date) ? (this.state.events[venue.id][this.props.date].event_type == 'Open Mic' ? OPEN_MIC_COLOR : SHOW_COLOR) : NO_EVENT_COLOR),
-            }}
-            radius={60}
-          >
-            <Tooltip>
-              <h2 className='venue-name'>{venue.name}</h2>
-              <hr />
-              <p className='venue-address'>{venue.address}</p>
-              <p className='venu-description'>"{venue.description}"</p>
-              {this.hasShow(venue, this.props.date) &&
-              <div>
-                <hr />
-                <b>SHOW TONIGHT!</b>
-                <div className='show-info'>
-                  <p className='show-title'>{this.state.events[venue.id][this.props.date].title}</p>
-                  <p className='show-time'>Music Starts at {this.formatTime(this.state.events[venue.id][this.props.date].start_time)}</p>
-                  <p className='show-price'>{this.getTicketPrice(this.state.events[venue.id][this.props.date])}</p>
-                  <p className='show-price'>{this.state.events[venue.id][this.props.date].ticket_price}</p>
-                </div>
-              </div>
-              }
-            </Tooltip>
-          </Circle>
-        ))}
-      </MapContainer>
-    );
-  }
+const Map = ({ date }) => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={false}
+    >
+      <MapData date={date} />
+    </MapContainer>
+  );
 }
 
 export default Map;
