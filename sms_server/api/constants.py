c65c603898ff6ae67c722bbe1b0dd476c00db130
admiralbolt <aviknecht@gmail.com>
2023-11-22 20:26:33 -0800

    Constants helper methods.

--- sms_server/api/constants.py
+++ sms_server/api/constants.py
@@ -1,12 +1,30 @@
 """Some things always change, but one thing stays the same, constants."""
 # pylint: disable=invalid-name
 
-class EventTypes:
+class Choices:
+  """Helper class for choices of things!"""
+
+  def all(self) -> list[str]:
+    """All choices!"""
+    choices = []
+    for attr in dir(self):
+      if not attr.isupper():
+        continue
+
+      val = getattr(self, attr)
+      choices.append(val)
+    return choices
+
+  def get_choices_for_db(self) -> list[tuple[str, str]]:
+    """Get a list of choices for models.py based on the attrs of the input."""
+    return [(val, val) for val in self.all()]
+
+class EventTypes(Choices):
   OPEN_MIC = "Open Mic"
   OPEN_JAM = "Open Jam"
   SHOW = "Show"
 
-class IngestionApis:
+class IngestionApis(Choices):
   AXS = "AXS"
   CRAWLER = "Crawler"
   EVENTBRITE = "Eventbrite"
@@ -16,13 +34,13 @@ class IngestionApis:
   TIXR = "TIXR"
   VENUEPILOT = "Venuepilot"
 
-class OpenMicTypes:
+class OpenMicTypes(Choices):
   ALL = "All"
   COMEDY = "Comedy"
   MUSIC = "Music"
   SPOKEN_WORD = "Spoken Word"
 
-class VenueTypes:
+class VenueTypes(Choices):
   BAR = "Bar"
   COFFEE_SHOP = "Coffee Shop"
   EVENT_SPACE = "Event Space"
