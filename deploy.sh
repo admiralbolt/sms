ed79b71fdf8572e090892d9314619b0c877fdc5d
Avi Knecht <aviknecht@gmail.com>
2023-11-24 23:13:51 +0000

    Deploy v1

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/sh
+
+git_base=/home/admiralbolt/git/sms/
+deployment_base=/home/admiralbolt/deployments/
+today=$(date '+%Y-%m-%d_%H%M%S')
+
+# 0. Pull the latest changes! (forgot about this...)
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/git_rsa
+cd $git_base
+git pull
+
+# 1. Create a new dated directory to put the deployment in.
+mkdir $deployment_base/$today
+
+# 2. Copy all backend files over to the deployment folder.
+#    IDK why I thought renaming it to backend was a good idea.
+cp -r $git_base/sms_server $deployment_base/$today/sms_backend
+# And make the logs folder!
+mkdir $deployment_base/$today/sms_backend/logs
+
+# Activate virtualenv and collect static files.
+source $deployment_base/venv/activate
+cd $deployment_base/$today/sms_backend/
+python manage.py collectstatic --noinput
+
+# 3. Create a build of the frontend, then copy it to deployment folder.
+cd $git_base/sms_frontend
+npm run build
+cp -r $git_base/sms_frontend/build $deployment_base/$today/sms_frontend
+
+# 4. Duplicate the latest version of the db / local settings.
+cp $deployment_base/latest/sms_backend/db.sqlite3 $deployment_base/$today/sms_backend/db.sqlite3
+cp $deployment_base/latest/sms_backend/sms_server/local_settings.py $deployment_base/$today/sms_backend/sms_server/local_settings.py
+
+# 5. Update latest symlink to point to todays date.
+ln -sfn $deployment_base/$today $deployment_base/latest
+
+# 6. Restart the servers!
+sudo service uwsgi restart
+sudo service nginx restart
