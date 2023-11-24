43ab2f3361cc43dc79a22f656c2884c6cfe34955
admiralbolt <aviknecht@gmail.com>
2023-11-24 14:51:09 -0800

    Default to all selected!

--- sms_frontend/src/contexts/LocalStorageContext.js
+++ sms_frontend/src/contexts/LocalStorageContext.js
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import dayjs from 'dayjs';
+import { useEventTypes, useVenueTypes } from '../hooks/api';
 
 const loadFromStorage = (key, defaultValue) => {
   const item = localStorage.getItem(key);
@@ -19,6 +20,8 @@ const loadDateFromStorage = (key, defaultValue) => {
 const LocalStorageContext = createContext();
 
 const LocalStorageContextProvider = ({ children }) => {
+  const eventTypes = useEventTypes();
+  const venueTypes = useVenueTypes();
   const [selectedEventTypes, setSelectedEventTypes] = useState(loadFromStorage('selectedEventTypes', []));
   const [selectedVenueTypes, setSelectedVenueTypes] = useState(loadFromStorage('selectedVenueTypes', []));
   const [selectedDate, setSelectedDate] = useState(loadDateFromStorage('selectedDate'));
@@ -34,6 +37,18 @@ const LocalStorageContextProvider = ({ children }) => {
   useEffect(() => {
     localStorage.setItem('selectedDate', selectedDate.format('YYYY-MM-DD'));
   }, [selectedDate]);
+
+  useEffect(() => {
+    if (selectedEventTypes.length == 0) {
+      setSelectedEventTypes(eventTypes);
+    }
+  }, [eventTypes]);
+
+  useEffect(() => {
+    if (selectedVenueTypes.length == 0) {
+      setSelectedVenueTypes(venueTypes);
+    }
+  }, [venueTypes]);
 
   return (
     <LocalStorageContext.Provider value={{ selectedEventTypes, setSelectedEventTypes, selectedVenueTypes, setSelectedVenueTypes, selectedDate, setSelectedDate }}>
