5d9a8b217b28017feba027c3120b7e4c5c8e0625
admiralbolt <aviknecht@gmail.com>
2024-04-11 12:31:31 -0700

    Cleanup some old stuff that's unused.

--- sms_server/api/management/commands/misc_debug.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from api.ingestion import venuepilot
-
-class Command(BaseCommand):
-
-  def handle(self, *args, **kwargs):
-    venuepilot.debug()
