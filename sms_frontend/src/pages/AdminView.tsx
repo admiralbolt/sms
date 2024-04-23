f3cc5a6ce879510deb2c3b941bf7e02e51df88b5
admiralbolt <aviknecht@gmail.com>
2024-04-22 20:08:56 -0700

    Create basic tab view for admin panel.

--- sms_frontend/src/pages/AdminView.tsx
+++ sms_frontend/src/pages/AdminView.tsx
@@ -1,19 +1,78 @@
 import { useIsAuthenticated } from "@/hooks/auth";
 
+import * as React from 'react';
+
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const AdminView = () => {
   const [isAuthenticated] = useIsAuthenticated();
 
-  const accessToken = localStorage.getItem("accessToken");
-  const refreshToken = localStorage.getItem("refreshToken");
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-    <div>
-      {(isAuthenticated) && <h1>YOU ARE AUTHENTICATED</h1>}
-       <br />
-       accessToken: {accessToken}
-       <br />
-       refreshToken: {refreshToken}
-    </div>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Status" {...a11yProps(0)} />
+          <Tab label="Commands" {...a11yProps(1)} />
+          <Tab label="Venues" {...a11yProps(2)} />
+          <Tab label="Open Mics" {...a11yProps(3)} />
+          <Tab label="Events" {...a11yProps(4)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Status!
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Commands!
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Venues!
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        Open Mics!
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={4}>
+        Events!
+      </CustomTabPanel>
+    </Box>
   );
 }
 
