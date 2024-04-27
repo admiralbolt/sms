d787b0069c16b32a4a22e914f5e0c66c15c5ca8a
Nate Spilman <nate.spilman@gmail.com>
2024-04-27 09:18:16 -0700

    reverting

--- sms_server/sms_server/settings.py
+++ sms_server/sms_server/settings.py
@@ -213,7 +213,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
   'REFRESH_TOKEN_LIFTIME': timedelta(days=1),
   'ROTATE_REFRESH_TOKENS': True,
   'BLACKLIST_AFTER_ROTATION': True
