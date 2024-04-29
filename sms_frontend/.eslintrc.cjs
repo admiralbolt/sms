e1cc2bd098d6d9ff61751b2057e4b8065adba00f
admiralbolt <aviknecht@gmail.com>
2024-04-29 00:31:38 -0700

    Update lint config to ignore unused vars/args that start with '_'

--- sms_frontend/.eslintrc.cjs
+++ sms_frontend/.eslintrc.cjs
@@ -10,9 +10,23 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_'
+      }
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_'
+      }
     ],
   },
 };
