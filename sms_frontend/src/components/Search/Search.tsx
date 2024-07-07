436d067af6dbad9c8e3606ee479e0540cde83f3b
admiralbolt <aviknecht@gmail.com>
2024-07-07 12:26:27 -0700

    Search now hooked up to the search events endpoint.

--- sms_frontend/src/components/Search/Search.tsx
+++ sms_frontend/src/components/Search/Search.tsx
@@ -1,46 +1,68 @@
-import Fuse, { FuseResult } from "fuse.js";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import { Box, List, ListItem, TextField, Typography } from "@mui/material";
+import { Box, CircularProgress, List, ListItem, TextField, Typography } from "@mui/material";
 
 import { EventCard } from "@/components/Events/EventCard";
-import { useFlatEvents, useVenueMap } from "@/hooks/flatFileApi";
 import { Event } from "@/types";
+
+import customAxios from "@/hooks/customAxios";
+
+import { useDebounce } from "use-debounce";
 
 const MAX_RESULTS = 50;
 
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [, , allEventsList] = useFlatEvents();
-  const venueMap = useVenueMap();
+  const [keyword] = useDebounce(searchTerm, 1000);
+  const [results, setResults] = useState<Event[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const [matches, setMatches] = useState<FuseResult<Event>[]>([]);
-  const [fuse, setFuse] = useState<Fuse<Event>>();
+  const search = () => {
+    if (keyword.length == 0) return;
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const fuseOptions = {
-    isCaseSensitive: false,
-    includeScore: true,
-    shouldSort: true,
-    includeMatches: true,
-    findAllMatches: true,
-    minMatchCharLength: 2,
-    threshold: 0.2,
-    ignoreLocation: true,
-    ignoreFieldNorm: true,
-    keys: ["title"],
-  };
-
-  useEffect(() => {
-    setFuse(new Fuse(allEventsList, fuseOptions));
-  }, [allEventsList, fuseOptions]);
+    customAxios
+      .get("api/event_search", {
+        params: {
+          keyword: keyword,
+        },
+      })
+      .then((response) => {
+        setResults(response.data);
+        setLoading(false);
+      });
+  }
 
   useEffect(() => {
-    if (fuse == null) return;
+    search();
+  }, [keyword]);
 
-    const matchList = fuse.search(searchTerm);
-    setMatches(matchList.slice(0, MAX_RESULTS));
-  }, [fuse, searchTerm]);
+  const renderSearch = () => {
+    if (loading) {
+      return (
+        <CircularProgress sx={{marginTop: "1em"}} />
+      );
+    }
+
+    return (
+      <>
+        <Typography>
+          {results.length == MAX_RESULTS ? `${MAX_RESULTS}+` : results.length}{" "}
+          results
+        </Typography>
+        <List
+          sx={{
+            paddingBottom: "2rem",
+          }}
+        >
+          {results.map((result) => (
+            <ListItem key={`event-${result.id}`}>
+                <EventCard event={result} showDate={true} />
+            </ListItem>
+          ))}
+        </List>
+      </>
+    );
+  }
 
   return (
     <Box>
@@ -52,29 +74,10 @@ export const Search = () => {
         value={searchTerm}
         onChange={(event) => {
           setSearchTerm(event.target.value);
+          setLoading(true);
         }}
       />
-      {matches.length > 0 && (
-        <Typography>
-          {matches.length == MAX_RESULTS ? `${MAX_RESULTS}+` : matches.length}{" "}
-          results
-        </Typography>
-      )}
-      <List
-        sx={{
-          paddingBottom: "2rem",
-        }}
-      >
-        {matches.map((match) => (
-          <ListItem key={`event-${match.item.id}`}>
-            {venueMap?.[match?.item?.venue] ? (
-              <EventCard event={match.item} showDate={true} />
-            ) : (
-              <>Unable to find event</>
-            )}
-          </ListItem>
-        ))}
-      </List>
+      {renderSearch()}
     </Box>
   );
 };
