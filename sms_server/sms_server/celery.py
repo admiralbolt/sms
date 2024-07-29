6549169df7ee09fe9d580e36af0de7d78cd3f523
admiralbolt <aviknecht@gmail.com>
2024-07-28 17:29:31 -0700

    Remove last reference to generate_open_mic

--- sms_server/sms_server/celery.py
+++ sms_server/sms_server/celery.py
@@ -20,7 +20,5 @@ def setup_periodic_tasks(sender, **kwargs):
   """Setup periodic jobs to crawl & generate events."""
   # Import all data everyday at 3am.
   sender.add_periodic_task(crontab(hour=3, minute=0), import_and_clean, name="Import & Clean Data")
-  # Generate open mic events once a week, sunday at 1am.
-  sender.add_periodic_task(crontab(day_of_week="sun", hour=1, minute=0), generate_open_mic_events, name="Generate Open Mics")
   # Delete old ingestion runs once a day.
   sender.add_periodic_task(crontab(hour=2, minute=0), delete_old_ingestion_runs, name="Delete Old Ingestion Runs")
