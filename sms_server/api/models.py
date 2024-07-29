1fec014e133b4ae1dcb96aab78b5472ec5467a99
admiralbolt <aviknecht@gmail.com>
2024-07-28 17:15:42 -0700

    Move open mic generator into the carpenter, and make the event_utils work!

--- sms_server/api/models.py
+++ sms_server/api/models.py
@@ -166,7 +166,7 @@ class Event(models.Model):
   finalized = models.BooleanField(default=False)
 
   def __str__(self):
-    return f"{self.title} ({self.venue.name}, {self.event_day}, {self.title})"
+    return f"|{self.id}| {self.title} ({self.venue.name}, {self.event_day}, {self.title})"
   
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
--- sms_server/api/tasks.py
+++ sms_server/api/tasks.py
@@ -12,27 +12,6 @@ from api.utils import open_mic_utils
 logger = logging.getLogger(__name__)
 
 @shared_task
-def generate_open_mic_events(name_filter: str="", max_diff: datetime.timedelta = datetime.timedelta(days=30), debug: bool=False):
-  """Generate events for open mics."""
-  if name_filter:
-    mic = open_mic_utils.get_open_mic_by_venue_name(name_filter)
-    if not mic:
-      print(f"Couldn't find open mic with name=({name_filter})")
-      return
-
-  open_mics = [mic] if name_filter else OpenMic.objects.all()
-  for mic in open_mics:
-    # Make sure we only generate events for mics that have a venue attached.
-    if not mic.venue:
-      continue
-
-    # Make sure we only generate events for mics that have generate_events set.
-    if not mic.generate_events:
-      continue
-
-    open_mic_utils.generate_open_mic_events(mic, max_diff=max_diff, debug=debug)
-
-@shared_task
 def import_and_clean(debug: bool=False):
   """Import data from ALL APIs & Crawlers."""
   ingester = Ingester()
