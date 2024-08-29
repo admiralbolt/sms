a40a188c392e40c9db8d95fbbf6520dd246ab4b6
admiralbolt <aviknecht@gmail.com>
2024-08-28 21:22:17 -0700

    Add 45 second timeout for individual browser runs. We'll see how often this happens in practice.

--- sms_server/api/utils/crawler_utils.py
+++ sms_server/api/utils/crawler_utils.py
@@ -1,13 +1,16 @@
 """Utilities for crawling things!"""
+import concurrent.futures
+import logging
 from typing import Optional
 
 import bs4
 from seleniumbase import Driver
 
+logger = logging.getLogger(__name__)
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 
-def get_html_soup(url: str) -> Optional[bs4.BeautifulSoup]:
+def get_html_soup_thread(url: str) -> Optional[bs4.BeautifulSoup]:
   """Delicious soup."""
   driver = Driver(uc=True, headless=True)
   soup = None
@@ -18,3 +21,14 @@ def get_html_soup(url: str) -> Optional[bs4.BeautifulSoup]:
     driver.quit()
 
   return soup
+
+def get_html_soup(url: str, timeout: int=45) -> Optional[bs4.BeautifulSoup]:
+  """Delicious soup."""
+  try:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+      future = executor.submit(get_html_soup_thread, url)
+      result = future.result(timeout)
+      return result
+  except concurrent.futures.TimeoutError:
+    logger.warning("Trawler Timeout. URL: %s, timeout: %s", url, timeout)
+  return None
